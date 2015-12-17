26e9ebb0db5e90a689b0c68fe8c3290208240653
RaviKumarBH <bhchinna578@gmail.com>
2015-12-17 13:08:23 +0530

    my commit

--- scripts/ravi555.js
+++ scripts/ravi555.js
@@ -1,4 +1,9 @@
-var http = require("http");
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var http = require("http");
 var ip = request.headers["x-forwarded-for"];
 var iplookupUrl = "http://ip-api.com/json/" + ip;
 var response = http.request({url: iplookupUrl});
@@ -13,6 +18,5 @@ if(response.status == "200"){
 	var message= "Thank you for tweeting from scriptr.io. There are now " + storage.local[country].count +  " tweets from " + country +".";
 	var tweetResult = tweet(message);
 	return "You've just tweeted [" + message + "] from scriptr.io, you can check it <a href='http://twitter.com/scriptrdemo' target='_blank'>here</a>."
-}else{
-	return "Ooops, we can't figure out your location!";
 }			                
+			
