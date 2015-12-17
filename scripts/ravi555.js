fc9dc59e8681bfd43e4f5cf858f80abfcf4c174a
RaviKumarBH <bhchinna578@gmail.com>
2015-12-17 13:06:35 +0530

    my commit for ravi555

--- scripts/ravi555.js
+++ scripts/ravi555.js
@@ -4,15 +4,3 @@ write=nobody
 execute=authenticated 
   **/ 
  //my scropt			
- /** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- //my scropt			
- /** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- //my scropt			ravi
