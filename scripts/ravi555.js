61686bf8c35d526eebbd307f3a93a69e5291fc0e
RaviKumarBH <bhchinna578@gmail.com>
2015-12-17 13:04:46 +0530

    Update ravi555.js

--- scripts/ravi555.js
+++ scripts/ravi555.js
@@ -4,3 +4,15 @@ write=nobody
 execute=authenticated 
   **/ 
  //my scropt			
+ /** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ //my scropt			
+ /** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ //my scropt			ravi
