d39eedbc9c3a47ef89ac985e960f885c36c95c68
RaviKumarBH <bhchinna578@gmail.com>
2016-01-22 16:04:18 +0530

    cmt

---
+++ scripts/blockyscript.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="33" inline="false" x="1" y="418"><value name="return"><block type="text" id="48"><field name="TEXT">ravi</field></block></value></block></xml>*#*#*/
+return 'ravi'			
