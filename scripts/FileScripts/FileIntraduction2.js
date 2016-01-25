e3a0b16a18728d57345ad3cbcc67f3dbf0d7a0a6
RaviKumarBH <bhchinna578@gmail.com>
2016-01-25 15:53:17 +0530

    dfdf

--- scripts/FileScripts/FileIntraduction2.js
+++ scripts/FileScripts/FileIntraduction2.js
@@ -3,5 +3,5 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":""},"scriptrdata":[]}}*#*#*/
-var content= ''; var response = apsdb.httpRespond(); response.write(content);response.close();			
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"//myscript"},"scriptrdata":[]}}*#*#*/
+var content= '//myscript'; var response = apsdb.httpRespond(); response.write(content);response.close();			
