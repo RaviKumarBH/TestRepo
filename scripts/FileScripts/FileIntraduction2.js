3e5594881c7e56b62a09fbfecc3821661b6152fe
RaviKumarBH <bhchinna578@gmail.com>
2016-01-25 15:48:42 +0530

    My Commit

---
+++ scripts/FileScripts/FileIntraduction2.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":""},"scriptrdata":[]}}*#*#*/
+var content= ''; var response = apsdb.httpRespond(); response.write(content);response.close();			
