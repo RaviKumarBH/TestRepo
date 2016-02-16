2c6fd1680c53163cbd20f01b343094def4efdb84
RaviKumarBH <bhchinna578@gmail.com>
2016-02-16 20:03:25 +0530

    dfgdfgtdfg

---
+++ scripts/BlockyScripts/CreateObject.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="lists_create_with" id="35" inline="false" x="1" y="60"><mutation items="3"></mutation><value name="ADD0"><block type="text" id="50"><field name="TEXT">ravi</field></block></value><value name="ADD1"><block type="text" id="51"><field name="TEXT">sdfsdf</field></block></value><value name="ADD2"><block type="text" id="52"><field name="TEXT">sdfsdfwwe</field></block></value></block></xml>*#*#*/
+['ravi', 'sdfsdf', 'sdfsdfwwe'];
+			
