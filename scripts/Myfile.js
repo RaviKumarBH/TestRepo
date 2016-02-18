73007d464e047afed894f7ad2cdccea4f0d77b82
RaviKumarBH <bhchinna578@gmail.com>
2016-02-18 12:16:46 +0530

    Update Myfile.js
    @auto-deploy

--- scripts/Myfile.js
+++ scripts/Myfile.js
@@ -4,4 +4,6 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"//myfile is in"},"scriptrdata":[]}}*#*#*/
-var content= '//myfile is in'; var response = apsdb.httpRespond(); response.write(content);response.close();			
+var content= '//myfile is in'; var response = apsdb.httpRespond(); response.write(content);response.close();		
+
+var a=10;
