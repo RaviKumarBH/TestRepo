87d85f440f8e3833d831cb01e9621404d0d780de
RaviKumarBH <bhchinna578@gmail.com>
2016-05-31 12:08:39 +0530

    test

---
+++ scripts/AAA/aaamyfile12345564456.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*SCRIPTR_PLUGIN*#*{"metadata":{"name":"CodeMirrorArbitraryFile","plugindata":{"fileData":"//sdlfkjsldfjsldfjs"},"scriptrdata":[]}}*#*#*/
+var content= '//sdlfkjsldfjsldfjs';  response.write(content);response.close();			
